7f5e3ba96fe448e555d4c89b07716afedaf9ec2e
hieuttbk <hieuttbk@gmail.com>
2021-05-27 09:02:21 +0700

    init

---
+++ WorkerApplication-1/src/main/java/WorkerApplicationLocal/CommonVar.java
@@ -0,0 +1,28 @@
+package WorkerApplicationLocal;
+
+public class CommonVar {
+	private CommonVar(){}
+	
+	public static final String ORIGINATOR="admin:admin";
+	public static final String CSEPROTOCOL="http";
+	public static final String WORKERCSEIP = "192.168.1.140";
+	public static final int WORKERCSEPORT = 8181;
+	public static final String WORKERCSEID = "worker-1-id";
+	public static final String WORKERCSENAME = "worker-1";
+	
+	public static final String AENAME = "WorkerAE";
+	public static final String AEPROTOCOL="http";
+	public static final String AEIP = "192.168.1.140";
+	public static final int AEPORT = 1500;	
+	public static final String AESUB="WorkerSub";
+	
+	public static final String WORKERCSEPOA = CSEPROTOCOL+"://"+WORKERCSEIP+":"+WORKERCSEPORT;
+	public static final String APPPOA = AEPROTOCOL+"://"+AEIP+":"+AEPORT;
+	public static final String NU = "/"+WORKERCSEID+"/"+WORKERCSENAME+"/"+AENAME;
+ 
+	public static final String[] CONTAINER = {"COMMAND", "MONITOR", "SERVICE", "RESULT", "DATA"};
+	public static final String[] SUBCONTAINER = {"COMMAND"};
+	
+	
+	public static final String DATADIR = "/home/pi/data/cut_image";
+}
---
+++ WorkerApplication-1/src/main/java/WorkerApplicationLocal/TestEncoder.java
@@ -0,0 +1,52 @@
+package WorkerApplicationLocal;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+
+public class TestEncoder {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		for(int i = 5; i <= 1400; i+=5){
+			Date now = (Calendar.getInstance()).getTime() ;
+			Timestamp ts = new Timestamp(now.getTime());
+			String ratioImages = "1-"+i;
+			ZipUtils.ZipData(ratioImages, "ABC");
+			//System.out.println("Finish zipping "+ ratioImages);
+			now = (Calendar.getInstance()).getTime();
+			long time = (new Timestamp(now.getTime())).getTime() - ts.getTime();
+			System.out.println("Time zipping " + ratioImages + " is " + time);
+		}
+		/*
+		 * 
+		
+		Date now = (Calendar.getInstance()).getTime() ;
+		Timestamp ts = new Timestamp(now.getTime());
+		for(int i = 5; i <= 1400; i+=5){
+			Date now = (Calendar.getInstance()).getTime() ;
+			Timestamp ts = new Timestamp(now.getTime());
+			String encodedString = "";
+			String dataName = "image1-"+i+"_ABC";
+			try {		
+				byte[] fileContent = FileUtils.readFileToByteArray(new File(CommonVar.DATADIR + "/" + dataName+".zip"));
+				encodedString = Base64.getEncoder().encodeToString(fileContent);
+				now = (Calendar.getInstance()).getTime();
+				long time = (new Timestamp(now.getTime())).getTime() - ts.getTime();
+				System.out.println("Time encoding " + dataName + " is " + time);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		 */
+		
+	}
+
+}
